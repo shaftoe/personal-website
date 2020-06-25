1a55d53ee5d8b6bf0d051879028151e8e7cdbd7e
Alex <shaftoe@users.noreply.github.com>
2020-06-25 21:07:57 +0300

    wip

--- assets/js/index-tab.js
+++ assets/js/index-tab.js
@@ -1,7 +1,20 @@
 'use strict'
 
+const anchorGroups = [
+    getAnchorsFromRowNumber(1),
+    getAnchorsFromRowNumber(2),
+]
+const sectionsGroups = [
+    getSectionsFromRowNumber(1),
+    getSectionsFromRowNumber(2),
+]
+
 function getAnchorsFromRowNumber(rowNumber) {
     return document.querySelectorAll(`#row_${rowNumber} .select-tab > a`)
+}
+
+function getSectionsFromRowNumber(rowNumber) {
+    return document.querySelectorAll(`#row_${rowNumber} section`)
 }
 
 function selected(elements) {
@@ -10,11 +23,6 @@ function selected(elements) {
     }
 }
 
-const anchorGroups = [
-    getAnchorsFromRowNumber(1),
-    getAnchorsFromRowNumber(2),
-]
-
 function isSelected(element) {
     return element.classList.contains('selected')
 }
@@ -22,17 +30,26 @@ function isSelected(element) {
 function selectClick(event, anchorIndex, groupIndex) {
     // swap "selected" class
     event.srcElement.classList.add("selected")
-    anchorGroups[groupIndex][anchorIndex % 2].classList.remove("selected")
+    anchorGroups[groupIndex][(anchorIndex+1) % 2].classList.remove("selected")
 
     // swap section visibility
+    sectionsGroups[groupIndex][(anchorIndex+1)%2].hidden = true
+    sectionsGroups[groupIndex][anchorIndex%2].hidden = false
 }
 
 
 /**
  * Add event listener for "click" event to every "tab' anchor
+ * and remove href to avoid unwanted scrolling
  */
 anchorGroups.forEach((group, groupIndex) => {
     group.forEach((anchor, anchorIndex) => {
-        anchor.addEventListener("click", (event) => { selectClick(event, anchorIndex+1, groupIndex) }, false)
+        anchor.removeAttribute("href");
+        anchor.addEventListener("click", (event) => { selectClick(event, anchorIndex, groupIndex) }, false)
     })
 })
+
+/**
+ * Hide second section when loaded
+ */
+sectionsGroups.forEach(sections => { if (sections[1]) sections[1].hidden = true })
