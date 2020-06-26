2fd0d23dfb05b0886488fbeda6302ac0857db55a
Alex <shaftoe@users.noreply.github.com>
2020-06-26 08:39:03 +0300

    wip

--- assets/js/index-tab.js
+++ assets/js/index-tab.js
@@ -1,13 +1,13 @@
 'use strict'
 
 const anchorGroups = [
-    getAnchorsFromRowNumber(1),
-    getAnchorsFromRowNumber(2),
-]
-const sectionsGroups = [
-    getSectionsFromRowNumber(1),
-    getSectionsFromRowNumber(2),
-]
+        getAnchorsFromRowNumber(1),
+        getAnchorsFromRowNumber(2),
+    ],
+    sectionsGroups = [
+        getSectionsFromRowNumber(1),
+        getSectionsFromRowNumber(2),
+    ]
 
 function getAnchorsFromRowNumber(rowNumber) {
     return document.querySelectorAll(`#row_${rowNumber} .select-tab > a`)
@@ -17,39 +17,34 @@ function getSectionsFromRowNumber(rowNumber) {
     return document.querySelectorAll(`#row_${rowNumber} section`)
 }
 
-function selected(elements) {
-    for (let element of elements) {
-        if (element.classList.contains('selected')) return element
-    }
-}
+window.onload = () => {
+    /**
+     * Hide second section for each row and elements meant for browsers
+     * with disabled JS
+     */
+    sectionsGroups.forEach(sections => { if (sections[1]) sections[1].hidden = true })
+    document.querySelectorAll('.noscript-only').forEach(element => element.hidden = true)
 
-function isSelected(element) {
-    return element.classList.contains('selected')
-}
+    /**
+     * Add event listener for "click" event to every "tab' anchor
+     * and remove href to avoid unwanted scrolling
+     */
+    anchorGroups.forEach((group, groupIndex) => {
+        group.forEach((anchor, anchorIndex) => {
+            anchor.removeAttribute("href"); // To avoid unnecessary scrolling
 
-function selectClick(event, anchorIndex, groupIndex) {
-    // swap "selected" class
-    event.srcElement.classList.add("selected")
-    anchorGroups[groupIndex][(anchorIndex+1) % 2].classList.remove("selected")
+            anchor.addEventListener("click", event => {
+                event.stopPropagation()
 
-    // swap section visibility
-    sectionsGroups[groupIndex][(anchorIndex+1)%2].hidden = true
-    sectionsGroups[groupIndex][anchorIndex%2].hidden = false
-}
+                // swap "selected" class
+                event.srcElement.classList.add("selected")
+                anchorGroups[groupIndex][(anchorIndex+1) % 2].classList.remove("selected")
 
+                // swap section visibility
+                sectionsGroups[groupIndex][(anchorIndex+1) % 2].hidden = true
+                sectionsGroups[groupIndex][anchorIndex % 2].hidden = false
 
-/**
- * Add event listener for "click" event to every "tab' anchor
- * and remove href to avoid unwanted scrolling
- */
-anchorGroups.forEach((group, groupIndex) => {
-    group.forEach((anchor, anchorIndex) => {
-        anchor.removeAttribute("href");
-        anchor.addEventListener("click", (event) => { selectClick(event, anchorIndex, groupIndex) }, false)
+            }, false)
+        })
     })
-})
-
-/**
- * Hide second section when loaded
- */
-sectionsGroups.forEach(sections => { if (sections[1]) sections[1].hidden = true })
+}
