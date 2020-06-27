cc5ed30964eb8c80e773e73654880d4c22ae6319
Alex <shaftoe@users.noreply.github.com>
2020-06-27 11:57:23 +0300

    wip

--- assets/js/index-tab.js
+++ assets/js/index-tab.js
@@ -1,7 +1,7 @@
 'use strict'
 
-function setVisibilityAll(selector, visible = true) {
-    document.querySelectorAll(selector).forEach(el => el.hidden = !visible)
+function setVisibilityAll(selector, visible = true, docRoot = document) {
+    docRoot.querySelectorAll(selector).forEach(el => el.hidden = !visible)
 }
 
 function hideSection(colNumber) {
@@ -20,46 +20,30 @@ function showNavsAnchors() {
     setVisibilityAll("nav.select-tab a", true)
 }
 
-function columnFromElement(element) {
-    let column
+function numberFromElementClassName(element, string) {
+    let num
     element.classList.forEach(cls => {
-        if (cls.match(/^col_/)) column = cls.substr(4)
+        if (cls.match(`${string}_`)) num = cls.substr(4)
     })
-    return column
-}
-
-function rowFromElement(element) {
-    let column
-    element.classList.forEach(cls => {
-        if (cls.match(/^row_/)) column = cls.substr(4)
-    })
-    return column
+    return num
 }
 
 function clickHandler(event) {
     event.stopPropagation()
 
-    const selectedCol = columnFromElement(event.srcElement)
-    const selectedRow = rowFromElement(event.srcElement)
-    const selectedSection = document.querySelector(`section.col_${selectedCol}.row_${selectedRow}`)
+    const selectedCol = numberFromElementClassName(event.srcElement, 'col')
+    const selectedRow = numberFromElementClassName(event.srcElement, 'row')
+    const unselectedRow = otherColumnNumber(selectedCol)
 
     // Set selected anchor and show selected section
     event.srcElement.classList.add("selected")
-    selectedSection.hidden = false
+    document.querySelector(`section.col_${selectedCol}.row_${selectedRow}`).hidden = false
 
     // Hide non-selected section
-    document.querySelectorAll("nav.select-tab a").forEach(anchor => {
-        if (anchor !== event.srcElement) anchor.classList.remove("selected")
-    })
-    document.querySelector(
-        `section.row_${selectedRow}.col_${otherColumnNumber(selectedCol)}`)
-        .hidden = true
+    document.querySelector(`a.row_${selectedRow}.col_${unselectedRow}`).classList.remove("selected")
+    document.querySelector(`section.row_${selectedRow}.col_${unselectedRow}`).hidden = true
 }
 
-/**
- * Add event listener for "click" event to every "tab" anchor
- * and remove href to avoid unwanted scrolling
- */
 function addEventListeners() {
     document.querySelectorAll('nav.select-tab a').forEach(anchor =>
         anchor.addEventListener("click", clickHandler, false))
@@ -75,48 +59,46 @@ function otherColumnNumber(columnNumber) {
     return (Number(columnNumber) % 2) + 1
 }
 
-function setActiveSection(columnNumber) {
+function setSessionActive(columnNumber) {
     document.querySelectorAll(`nav.select-tab a.col_${columnNumber}`)
         .forEach(anchor => anchor.classList.add("selected"))
 }
 
-function unsetActiveSection(columnNumber) {
+function unsetSessionActive(columnNumber) {
     document.querySelectorAll(`nav.select-tab a.col_${columnNumber}`)
         .forEach(anchor => anchor.classList.remove("selected"))
-}
-
-function hideLargeScreen() {
-    setVisibilityAll('.large-only', false)
-}
-
-function showLargeScreen() {
-    setVisibilityAll('.large-only', true)
 }
 
 function handleMediaQueryEvent(event) {
     if (event.matches) {
         addEventListeners()
-        setActiveSection(1)
-        unsetActiveSection(2)
+        setSessionActive(1)
+        unsetSessionActive(2)
         hideSection(2)
-        hideLargeScreen()
-        showNavsAnchors()
+        setVisibilityAll('.large-only', false)
+        setVisibilityAll('.small-only', true)
     } else {
         removeEventListeners()
         showSection(1)
         showSection(2)
-        unsetActiveSection(1)
-        unsetActiveSection(2)
-        hideNavsAnchors()
-        showLargeScreen()
+        unsetSessionActive(1)
+        unsetSessionActive(2)
+        setVisibilityAll('.large-only', true)
+        setVisibilityAll('.small-only', false)
     }
 }
 
 window.onload = () => {
-    // JS is enabled so let's hide all the noscript-only elements
-    // document.querySelectorAll('.noscript-only').forEach(element => element.hidden = true)
-
     const mql = window.matchMedia("screen and (max-width: 799px)") // FIXME fetch from CSS
     handleMediaQueryEvent(mql)
     mql.addListener(handleMediaQueryEvent)
+}
+
+function getStyleSheet(unique_title) {
+  for (var i=0; i<document.styleSheets.length; i++) {
+    var sheet = document.styleSheets[i];
+    if (sheet.title == unique_title) {
+      return sheet;
+    }
+  }
 }
