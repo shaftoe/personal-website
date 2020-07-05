59ed12604dc7477f8b10cefd47f94601d0ba2f12
Alex <shaftoe@users.noreply.github.com>
2020-07-05 15:11:43 +0300

    wip

--- assets/js/index-tab.js
+++ assets/js/index-tab.js
@@ -1,107 +1,7 @@
 "use strict"
 
-function setVisibilityAll(selector, visible = true, docRoot = document) {
-    docRoot.querySelectorAll(selector).forEach(el => el.hidden = !visible)
-}
-
-function hideSection(colNumber) {
-    setVisibilityAll(`section.col_${colNumber}`, false)
-}
-
-function showSection(colNumber) {
-    setVisibilityAll(`section.col_${colNumber}`, true)
-}
-
-function numberFromElementClassName(element, string) {
-    let num
-    element.classList.forEach(cls => {
-        if (cls.match(`${string}_`)) num = cls.substr(4)
-    })
-    return num
-}
-
-function clickHandler(event) {
-    event.stopPropagation()
-
-    const selectedCol = numberFromElementClassName(event.srcElement, "col")
-    const selectedRow = numberFromElementClassName(event.srcElement, "row")
-    const unselectedCol = flipNumber(selectedCol)
-
-    event.srcElement.classList.add("selected")
-    document.querySelector(`nav.select_tab a.row_${selectedRow}.col_${unselectedCol}`)
-        .classList.remove("selected")
-
-    document.querySelector(`section.row_${selectedRow}.col_${selectedCol}`).hidden = false
-    document.querySelector(`section.row_${selectedRow}.col_${unselectedCol}`).hidden = true
-}
-
-function addEventListeners() {
-    document.querySelectorAll("nav.select_tab a").forEach(anchor =>
-        anchor.addEventListener("click", clickHandler, false))
-}
-
-function removeEventListeners() {
-    document.querySelectorAll("nav.select_tab a").forEach(anchor =>
-        anchor.removeEventListener("click", clickHandler, false))
-}
-
-/**
- * Return 1 if number is 2, return 2 if number is 1
- * @param {integer} number
- */
-function flipNumber(number) {
-    if (number < 1 || number > 2)
-        console.error("Wrong input: must be either 1 or 2, got", number)
-    return (Number(number) % 2) + 1
-}
-
-function setSessionActive(columnNumber) {
-    document.querySelectorAll(`nav.select_tab a.col_${columnNumber}`)
-        .forEach(anchor => anchor.classList.add("selected"))
-}
-
-function unsetSessionActive(columnNumber) {
-    document.querySelectorAll(`nav.select_tab a.col_${columnNumber}`)
-        .forEach(anchor => anchor.classList.remove("selected"))
-}
-
-function handleMediaQueryEvent(event) {
-    if (event.matches) {
-        setSessionActive(1)
-        unsetSessionActive(2)
-        hideSection(2)
-        addEventListeners()
-    } else {
-        showSection(1)
-        showSection(2)
-        unsetSessionActive(1)
-        unsetSessionActive(2)
-        removeEventListeners()
-    }
-}
-
-/**
- * Get CSS --responsive-max-width variable defined in :root from latest inline CSS document
- */
-function getMaxResponsiveWidth() {
-    const len = document.styleSheets.length,
-        sheet = document.styleSheets[len - 1]
-    return sheet.rules[0].cssText.match(/--responsive-max-width: (\d+px)/).pop()
-}
-
-// window.onload = () => document.querySelectorAll("section")
-//     .forEach(section => section.classList.add("animated"))
-
-// const mql = window.matchMedia(`screen and (max-width: ${getMaxResponsiveWidth()})`)
-// handleMediaQueryEvent(mql)
-// mql.addListener(handleMediaQueryEvent)
-
-//Get the button:
 const scrollToTop = document.querySelector(".scroll_to_top");
 scrollToTop.style.display = "none"
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -110,3 +10,6 @@ function scrollFunction() {
         scrollToTop.style.display = "none"
     }
 }
+
+// When the user scrolls down 20px from the top of the document, show the scroll link
+window.onscroll = function() {scrollFunction()};
